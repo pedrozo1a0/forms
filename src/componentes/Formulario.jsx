eb6a5b640725c2594aec5749adc475246fe7d7ef
pedrozo1a0 <ale.pedrozo.952@gmail.com>
2024-05-22 06:41:54 -0400

    first commit

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -9,38 +9,81 @@ export function Formulario (props){
         confirmPassword: ""
     };
     const [ficha, setFicha]= useState(fichaInicial);
-    const actualizarFicha=(valorNuevo, campo)=>{
-        setFicha( {...ficha, [campo]: valorNuevo} );
-        
-    }
+    
     const agregarInformacion=(e)=>{
         e.preventDefault();
         props.setInformaciones([...props.informaciones, ficha]);
         console.log(props.informaciones);
     }
+
+    const [longCaracteresName, setLongCaracteresName]= useState(0);
+    const [longCaracteresLastName, setLongCaracteresLastName]= useState(0);
+    const [longCaracteresEmail, setLongCaracteresEmail]= useState(0);
+    const [longCaracteresPassword, setLongCaracteresPassword]= useState(0);
+    const [passwordValue, setPasswordValue]= useState("");
+    const [confirmPasswordValue, setConfirmPasswordValue]= useState("");
+
+
+
+    const actualizarFicha=(valorNuevo, campo)=>{
+        setFicha( {...ficha, [campo]: valorNuevo} );
+    }
+
+    
+    const actualizarInfoName=(valor, campo, longitud, callback)=>{
+        callback(valor, campo, longitud);
+        setLongCaracteresName(longitud);
+    }
+    const actualizarInfoLastName=(valor, campo, longitud, callback)=>{
+        callback(valor, campo, longitud);
+        setLongCaracteresLastName(longitud);
+    }
+    const actualizarInfoEmail=(valor, campo, longitud, callback)=>{
+        callback(valor, campo, longitud);
+        setLongCaracteresEmail(longitud)
+    }
+    
+    const verificarPassword=(valor, campo, longitud, callback)=>{
+        callback(valor, campo, longitud);
+        setLongCaracteresPassword(longitud);
+        setPasswordValue(valor);
+        
+    }
+
+    const actualizarInfoPassword=(valor, campo,  callback)=>{
+        callback(valor, campo, 0 );
+        console.log(valor==ficha.password);
+        setConfirmPasswordValue(valor);
+    }
+
     return(
         <>
         <form onSubmit={agregarInformacion}>
             <div className="container">
                 <div>
                     <label htmlFor="name" >First Name</label>
-                    <input type="text" id="name" onChange={(e)=>{actualizarFicha(e.target.value, "firstName" )}}  />
+                    <input type="text" id="name" onChange={(e)=>{actualizarInfoName(e.target.value, "firstName", e.target.value.length, actualizarFicha  )}}  />
+                    {longCaracteresName<2 ? <p>El campo debe tener por lo menos dos caracteres</p>  : ""}
                 </div>
                 <div>
                     <label htmlFor="lastName" >Last Name</label>
-                    <input type="text" id="lastName" onChange={(e)=>{actualizarFicha(e.target.value, "lastName" )}}/>
+                    <input type="text" id="lastName" onChange={(e)=>{actualizarInfoLastName(e.target.value, "lastName", e.target.value.length, actualizarFicha )}}/>
+                    {longCaracteresLastName<2 ? <p>El campo debe tener por lo menos dos caracteres</p>  : ""}
                 </div>
                 <div>
                     <label htmlFor="email" >Email</label>
-                    <input type="email" id="email" onChange={(e)=>{actualizarFicha(e.target.value, "email" )}}/>
+                    <input type="email" id="email" onChange={(e)=>{actualizarInfoEmail(e.target.value, "email", e.target.value.length, actualizarFicha )}}/>
+                    {longCaracteresEmail<5 ? <p>El campo debe tener por lo menos cinco caracteres</p>  : ""}
                 </div>
                 <div>
                     <label htmlFor="password" >Password</label>
-                    <input type="password" id="password" onChange={(e)=>{actualizarFicha(e.target.value, "password" )}}/>
+                    <input type="password" id="password" onChange={(e)=>{verificarPassword(e.target.value, "password", e.target.value.length, actualizarFicha )}}/>
+                    {longCaracteresPassword<8 ? <p>El campo debe tener por lo menos ocho caracteres</p>  : ""}
                 </div>
                 <div>
                     <label htmlFor="confirmPassword" >Confirm<br/>Password</label>
-                    <input type="password" id="confirmPassword" onChange={(e)=>{actualizarFicha(e.target.value, "confirmPassword" )}}/>
+                    <input type="password" id="confirmPassword" onChange={(e)=>{actualizarInfoPassword(e.target.value, "confirmPassword", actualizarFicha)}}/>
+                    {confirmPasswordValue===passwordValue  ? "" : <p>Password doesnt coincide</p>    }
                 </div>
             </div>
         <button>Enviar informacion</button>
